c5a4110f39da00a50cac8b61e180b1672474b03d
DanielLimberg <d.limberg@alumni.maastrichtuniversity.nl>
2016-02-29 09:57:07 +0100

    dynamically link file to source file

--- MaritimePiracy_Code/PiracyAllDescrStats.R
+++ MaritimePiracy_Code/PiracyAllDescrStats.R
@@ -3,8 +3,8 @@
 #######################
 
 #set working directories if necessary (if data lies in git repo it is not necessary though)
-try(setwd("E:/bjoer/Documents/GitHub/MaritimePiracyWrangling/Data"),silent=TRUE)
-try(setwd("C:/Users/Dani/Documents/GitHub2/MaritimePiracy/MaritimePiracy_Data"),silent=TRUE)
+try(setwd(""),silent=TRUE)
+try(setwd("C:/Users/Dani/Documents/GitHub2/MaritimePiracy/MaritimePiracy_Code"),silent=TRUE)
 getwd()
 
 # Dynamically Link to first R script file
