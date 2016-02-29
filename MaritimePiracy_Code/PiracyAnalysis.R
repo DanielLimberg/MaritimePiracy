b77ed7ec158f7f7db650dd28345f49c334ca7754
DanielLimberg <d.limberg@alumni.maastrichtuniversity.nl>
2016-02-29 17:03:54 +0100

    spelling correction
    - model names
    - NB3: dependent var

--- MaritimePiracy_Code/PiracyAnalysis.R
+++ MaritimePiracy_Code/PiracyAnalysis.R
@@ -12,10 +12,10 @@ source("MergeAll.R")
 
 #Logit model 1a
 summary(logit1 <- glm(incbinary ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + coastkm + battlelow, data = panel, family = "binomial"))
-summary(logit1 <- glm(incbinary ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + log(coastkm) + battlelow, data = panel, family = "binomial"))
-summary(logit2 <- glm(incbinary ~ cmort + FD + DD + poprur.gr + ((polity2)^2) + GNIpc + WTI + unem.y.m + coastkm + battlelow, data = panel, family = "binomial"))
+summary(logit2 <- glm(incbinary ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + log(coastkm) + battlelow, data = panel, family = "binomial"))
+summary(logit3 <- glm(incbinary ~ cmort + FD + DD + poprur.gr + ((polity2)^2) + GNIpc + WTI + unem.y.m + coastkm + battlelow, data = panel, family = "binomial"))
 
 #Neg. Bin. model 1a
-summary(ND1 <- glm.nb(incidents ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + coastkm + battlelow, data = panel))
-summary(ND1 <- glm.nb(incidents ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + log(coastkm) + battlelow, data = panel))
-summary(ND2 <- glm.nb(incbinary ~ cmort + FD + DD + poprur.gr + ((polity2)^2) + GNIpc + WTI + unem.y.m + coastkm + battlelow, data = panel))
+summary(NB1 <- glm.nb(incidents ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + coastkm + battlelow, data = panel))
+summary(NB2 <- glm.nb(incidents ~ cmort + FD + DD + pop.gr + ((polity2)^2) + GDPpc + WTI + unem.total + log(coastkm) + battlelow, data = panel))
+summary(NB3 <- glm.nb(incidents ~ cmort + FD + DD + poprur.gr + ((polity2)^2) + GNIpc + WTI + unem.y.m + coastkm + battlelow, data = panel))
